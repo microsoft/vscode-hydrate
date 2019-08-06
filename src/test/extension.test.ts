bb03e83ff7f55fe9157e2f97c96ec5c918f06a52
madelineliao <madeline.m.liao@gmail.com>
2019-08-06 16:20:55 -0700

    Unit tests and refactoring

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,22 +1,86 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
+import * as sinon from 'sinon';
+import * as Extension from '../extension';
+import * as KubeConfig from '../kubeconfig';
+import * as KubeHelpers from '../getKubeHelpers';
+import * as vscode from 'vscode';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function () {
+teardown(sinon.restore);
 
-    // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+suite("Test hydrateCluster", function () {
+
+    suite("With bad kubeconfig", function () {
+        test('VSCode should show error message', function () {
+            sinon.stub(KubeConfig, 'getKubeConfig').returns('bad kube config');
+
+            const hydrateClusterSpy = sinon.spy(Extension, 'hydrateCluster');
+            const showErrorMessage = sinon.spy(vscode.window, 'showErrorMessage');
+            
+            hydrateClusterSpy();
+
+            assert(hydrateClusterSpy.calledOnce);
+            assert(showErrorMessage.calledOnce);
+        });
     });
+
+    suite('With good kubeconfig', function () {
+        test('VSCode should create a terminal named \'hydrate\'', function () {
+            const hydrateClusterSpy = sinon.spy(Extension, 'hydrateCluster');
+            const createTerminal = sinon.spy(vscode.window, 'createTerminal');
+
+            hydrateClusterSpy();
+
+            assert(createTerminal.calledOnce);
+            assert(vscode.window.terminals[0].name === 'hydrate');
+        });
+
+    });
+
+});
+
+suite("Test getKubeConfig", function () {
+    
+
+    suite('If getVSCodeKubeConfig returns undefined', function () {
+        
+        test('Should return either the env kubeconfig or the default config', function () {
+            sinon.stub(KubeHelpers, 'getVscodeKubeConfig').returns(undefined);
+            const kubeconfig = KubeConfig.getKubeConfig();
+
+            assert(kubeconfig === KubeHelpers.getEnvKubeConfig() || kubeconfig === KubeHelpers.getDefaultKubeConfig());
+
+            sinon.restore();
+        });
+
+    });
+
+    suite('If getVSCodeKubeConfig returns a string', function () {
+       
+        test('Should return the VSCode kubeconfig', function () {
+            sinon.stub(KubeHelpers, 'getVscodeKubeConfig').returns('./resources/config');
+            const kubeconfig = KubeConfig.getKubeConfig();
+
+            assert(kubeconfig === KubeHelpers.getVscodeKubeConfig());
+
+            sinon.restore();
+        });
+
+    });
+    
+    suite('If both the VSCode kubeconfig and the env kubeconfig are undefined', function () {
+
+        test('Should return the default kubeconfig', function () {
+            sinon.stub(KubeHelpers, 'getVscodeKubeConfig').returns(undefined);
+            sinon.stub(KubeHelpers, 'getEnvKubeConfig').returns(undefined);
+            
+            const kubeconfig = KubeConfig.getKubeConfig();
+
+            assert(kubeconfig === KubeHelpers.getDefaultKubeConfig());
+
+            sinon.restore();
+        });
+
+    });
+    
 });
